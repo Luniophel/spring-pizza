e63794209f35903fe0b12ffdc33628c2493a4116
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-07-21 16:08:28 +0200

    add empty list notification, add repo, model and controller of Pizza

--- src/main/java/jana60/controller/menuController.java
+++ src/main/java/jana60/controller/menuController.java
@@ -1,13 +1,21 @@
 package jana60.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jana60.repository.PizzaRepository;
+
 @Controller
 @RequestMapping("/menu")
-public class menuController 
+public class MenuController 
 {
+	
+	@Autowired
+	private PizzaRepository repo;
+	
 	@GetMapping
 	public String menu()
 	{
@@ -18,9 +26,10 @@ public class menuController
 	}
 	
 	@GetMapping("/le-nostre-pizze")
-	public String pizzaList()
+	public String pizzaList(Model model)
 	{
 		
+		model.addAttribute("pizzaList", repo.findAll());
 		return "/sections/menu/pizzaList";
 		
 	}

574017051ea91c024df4725b23ff164193add855
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-07-27 16:25:04 +0200

    add model ingrediente

---
+++ src/main/java/jana60/repository/IngredienteRepository.java
@@ -0,0 +1,10 @@
+package jana60.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import jana60.model.Ingrediente;
+
+public interface IngredienteRepository extends CrudRepository<Ingrediente, Integer> 
+{
+
+}

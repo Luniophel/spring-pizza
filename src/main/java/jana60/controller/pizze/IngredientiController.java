129476aca3b5186caf0e5cd016f3de95c1e372c4
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-07-28 07:05:03 +0200

    add ingredienti remove btn, fixed typo, tested

--- src/main/java/jana60/controller/pizze/IngredientiController.java
+++ src/main/java/jana60/controller/pizze/IngredientiController.java
@@ -1,15 +1,22 @@
 package jana60.controller.pizze;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jana60.model.Ingrediente;
 import jana60.repository.IngredienteRepository;
@@ -38,6 +45,7 @@ public class IngredientiController
 		if (br.hasErrors())
 		{
 			
+			br.addError(new FieldError("ingrediente", "nome", "Non può essere vuoto!"));
 			model.addAttribute("ingredientiList", repo.findAll());
 			return "/sections/menu/pizza/pizzaIngredienti";
 
@@ -52,4 +60,25 @@ public class IngredientiController
 		
 	}
 	
+	//___COMANDI RIMUOVI INGREDIENTI___//
+
+		@GetMapping("/rimuovi/{id}")
+		public String delete(@PathVariable("id") Integer ingredienteId, RedirectAttributes ra)
+		{
+			
+			Optional<Ingrediente> result = repo.findById(ingredienteId);
+			if(result.isPresent())
+			{
+				
+				repo.deleteById(ingredienteId);
+				ra.addFlashAttribute("successMessage", "L'ingrediente " + result.get().getNome() + " è stato eliminato con successo.");
+				return "redirect:/menu/le-nostre-pizze/ingredienti";
+			
+			}
+			
+			else			
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza con ID" + ingredienteId + "non presente.");
+						
+		}
+		
 }
